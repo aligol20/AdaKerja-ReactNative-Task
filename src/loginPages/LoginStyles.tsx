598373b5fee84dc0a2ecb80002e6eed3314644a3
aligol20 <aligol20@gmail.com>
2020-05-26 04:21:47 +0430

    Change text align

--- src/loginPages/LoginStyles.tsx
+++ src/loginPages/LoginStyles.tsx
@@ -23,7 +23,7 @@ const login_styles = StyleSheet.create({
     borderRadius: 23,
     color: 'white',
     fontSize: 0.07 * width,
-    textAlign: 'left',
+    textAlign: 'center',
   },
   logo_text: {color: 'white'},
   help_text: {
