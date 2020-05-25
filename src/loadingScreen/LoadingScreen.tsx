b6a2d308a5b9b394d4cbc9b54b5a52719e0b878f
aligol20 <aligol20@gmail.com>
2020-05-26 03:43:40 +0430

    In this page we check AsyncStorage for userInfo

---
+++ src/loadingScreen/LoadingScreen.tsx
@@ -0,0 +1,58 @@
+/**
+ * In this page we check AsyncStorage for userInfo
+ */
+import LottieView from 'lottie-react-native';
+import React, {useEffect} from 'react';
+import {Dimensions, Text, View} from 'react-native';
+import SavingUserToSite from '../loginPages/SavingUserToSite';
+import AsyncStorage from '@react-native-community/async-storage';
+import {RouteState} from '../Mytypes';
+
+const width = Dimensions.get('screen').width;
+interface Props {
+  response(decide: RouteState): void;
+}
+
+/**
+ *
+ * @param response response - callBack for chosen route
+ *
+ */
+const LoadingScreen: React.FC<Props> = ({response}) => {
+  /**
+   * Loader first check AsyncStorage
+   * if there isn't any userInfo we call response('signIn') for going UserInput route
+   */
+  const Loader = () => {
+    const getToken = async () => {
+      const token = await AsyncStorage.getItem('userInfo');
+
+      if (!token) {
+        response(RouteState.signIn);
+      } else {
+        response(RouteState.logged);
+      }
+      return token;
+    };
+    /**
+     * getToken function is just for checking AsyncStorage
+     */
+    getToken();
+
+    return <></>;
+  };
+
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+      {Loader()}
+      <LottieView
+        style={{marginTop: 3, width: 0.15 * width, height: 0.15 * width}}
+        source={require('../lottie/loading__.json')}
+        autoPlay
+        loop={true}
+      />
+    </View>
+  );
+};
+
+export default LoadingScreen;
