2a9721889e40c61205bf376de9d5cdb10bc810c9
aligol20 <aligol20@gmail.com>
2020-05-26 03:37:49 +0430

    App starts from here

--- App.tsx
+++ App.tsx
@@ -1,118 +1,67 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
+ * ب
+ * ی  ز
+ *App starts from here
+ *Starts from Loading page
+ *Our Router is ReactNavigation v5
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React, {useState} from 'react';
+import 'react-native-gesture-handler';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {HomeChildren} from './src/homeRoute/HomeChildren';
+import {navigationRef} from './src/RootNavigation';
+import Loading from './src/loadingScreen/LoadingScreen';
+import LoginUser from './src/loginPages/LoginUser';
+import PasswordPage from './src/loginPages/PasswordPage';
+import {RouteState} from './src/Mytypes';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App: React.FC = () => {
+  console.disableYellowBox = true;
 
-declare const global: {HermesInternal: null | {}};
+  /**
+   * whatToDo is Loading page callback for decide correct route
+   */
 
-const App = () => {
+  const [whatToDo, setWhat] = useState<RouteState>(RouteState.loading);
+
+  /**
+   * response const is for login mutation
+   * get user from UserInput
+   */
+  const [response, setResponse] = useState<{user: string}>({
+    user: '',
+  });
+
+  /**
+   * Sets values got from UserInput page
+   * @param value - get 3 object from LoginUser page
+   * user: username
+   * decide: decides to go in PasswordPage or loading page
+   */
+  const LoginResponse = (value: {
+    user: string;
+    decide: RouteState.loading | RouteState.validation;
+  }) => {
+    setWhat(value.decide);
+    setResponse({user: value.user});
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaProvider>
+      <NavigationContainer ref={navigationRef}>
+        {whatToDo === RouteState.logged ? (
+          <HomeChildren />
+        ) : whatToDo === RouteState.signIn ? (
+          <LoginUser response={(value) => LoginResponse(value)} />
+        ) : whatToDo === RouteState.validation ? (
+          <PasswordPage loginInput={response} response={setWhat} />
+        ) : (
+          <Loading response={setWhat} />
+        )}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
 export default App;
