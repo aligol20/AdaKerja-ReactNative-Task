400b8d0d1eaf85e68ad201387e28517082022867
aligol20 <aligol20@gmail.com>
2020-05-26 12:56:27 +0430

    Pagination added to commits

--- src/ui/CommitsList.tsx
+++ src/ui/CommitsList.tsx
@@ -1,29 +1,23 @@
 /**
  * List of commits can be found here
  */
+import AsyncStorage from '@react-native-community/async-storage';
+import {useFocusEffect, useRoute} from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 import React, {useEffect, useState} from 'react';
 import {
-  View,
-  FlatList,
-  Text,
-  Image,
-  StyleSheet,
-  SectionList,
   Dimensions,
-  StatusBar,
   Platform,
+  SectionList,
+  StatusBar,
+  Text,
+  View,
 } from 'react-native';
-import LottieView from 'lottie-react-native';
-
-import {useFocusEffect, useRoute} from '@react-navigation/native';
-import {
-  TouchableHighlight,
-  TouchableOpacity,
-} from 'react-native-gesture-handler';
-import Modal from 'react-native-modal';
 import ListItem from './ListItem';
 import {commit_list} from './Styles';
+
 const CommitsList = () => {
+  const [page, setPage] = useState<number>(1);
   useFocusEffect(
     React.useCallback(() => {
       StatusBar.setBarStyle('light-content');
@@ -31,62 +25,76 @@ const CommitsList = () => {
     }, []),
   );
   const route = useRoute<any>();
-  const [commits, setCommits] = useState<{title: string; data: []}[]>([
-    {title: '', data: []},
-  ]);
+  const [commits, setCommits] = useState<any[]>([{title: '', data: []}]);
   const [loading, setLoading] = useState<boolean>(true);
-  const octokit = require('@octokit/rest')();
+  const [pass, setPass] = useState();
+  const [user, setUser] = useState();
+  const octokit = require('@octokit/rest')({
+    timeout: 0, // 0 means no request timeout
+    headers: {
+      accept: 'application/vnd.github.v3+json',
+      'user-agent': 'octokit/rest.js v1.2.3', // v1.2.3 will be current version
+    },
 
-  const param = route.params;
+    // custom GitHub Enterprise URL
+    baseUrl: 'https://api.github.com',
+
+    // Node only: advanced request options can be passed as http(s) agent
+    agent: undefined,
+  });
   const width = Dimensions.get('screen').width;
-  useEffect(() => {
-    octokit.repos
-      .getCommits({
-        owner: route.params.owner,
-        repo: route.params.repo,
-      })
-      .then(({data, headers, status}: any) => {
-        // handle data
-        let test: {title: string}[] = [];
-        let sectionList = [];
-        for (let i = 0; i < data.length; i++) {
-          if (i !== 0) {
-            if (
-              data[i - 1].commit.author.date.split('T')[0] !==
-              data[i].commit.author.date.split('T')[0]
-            ) {
-              test.push({title: data[i].commit.author.date.split('T')[0]});
-            }
-          } else {
-            test.push({title: data[i].commit.author.date.split('T')[0]});
-          }
-        }
-        if (test.length > 0) {
-          for (let i = 0; i < test.length; i++) {
-            console.log(
-              data
-                .filter(
-                  (x: any) =>
-                    test[i].title === x.commit.author.date.split('T')[0],
-                )[0]
-                .commit.message.split('\n\n'),
-              'ggggggggggg',
-            );
-            sectionList.push({
-              title: test[i].title,
-              data: data.filter(
-                (x: any) =>
-                  test[i].title === x.commit.author.date.split('T')[0],
-              ),
-            });
-          }
-        }
 
-        console.log(sectionList, 'test test qqqqqqqqq');
-        setCommits(sectionList);
-        setLoading(false);
-      });
-  }, []);
+  useEffect(() => {
+    AsyncStorage.getItem('userInfo').then((value) => {
+      if (value) {
+        const result = JSON.parse(value);
+        octokit.authenticate({
+          type: 'basic',
+          username: result.user,
+          password: result.pass,
+        });
+        octokit.repos
+          .getCommits({
+            owner: route.params.owner,
+            repo: route.params.repo,
+            page: page,
+          })
+          .then(({data, headers, status}: any) => {
+            // handle data
+            let test: {title: string}[] = [];
+            let sectionList = [];
+            for (let i = 0; i < data.length; i++) {
+              if (i !== 0) {
+                if (
+                  data[i - 1].commit.author.date.split('T')[0] !==
+                  data[i].commit.author.date.split('T')[0]
+                ) {
+                  test.push({title: data[i].commit.author.date.split('T')[0]});
+                }
+              } else {
+                test.push({title: data[i].commit.author.date.split('T')[0]});
+              }
+            }
+            if (test.length > 0) {
+              for (let i = 0; i < test.length; i++) {
+                sectionList.push({
+                  title: test[i].title,
+                  data: data.filter(
+                    (x: any) =>
+                      test[i].title === x.commit.author.date.split('T')[0],
+                  ),
+                });
+              }
+            }
+
+            let combinedArray = [...commits, ...sectionList];
+            setCommits(combinedArray);
+
+            setLoading(false);
+          });
+      }
+    });
+  }, [page]);
 
   return (
     <View style={{backgroundColor: '#4F68C4'}}>
@@ -110,8 +118,11 @@ const CommitsList = () => {
           sections={commits}
           keyExtractor={(item, index) => item + index.toString()}
           stickySectionHeadersEnabled={true}
+          onEndReachedThreshold={1}
+          onEndReached={(value) => setPage(page + 1)}
           renderItem={(item: any) => (
             <ListItem
+              key={item.index}
               message={item.item.commit.message}
               name={item.item.commit.author.name}
               email={item.item.commit.author.email}
