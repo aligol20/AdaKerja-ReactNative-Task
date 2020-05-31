6b3bdedafdd0847b01e91d5dabc2d6a6eea98a02
aligol20 <aligol20@gmail.com>
2020-05-31 19:07:06 +0430

    Inline css & actions removed

--- src/loginPages/LoginUser.tsx
+++ src/loginPages/LoginUser.tsx
@@ -11,9 +11,9 @@ import {
   View,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {RouteState} from '../Mytypes';
+import {RouteState} from '../types/Mytypes';
 import {common_styles} from '../styles/CommonStyle';
-import login_styles from './LoginStyles';
+import login_styles from '../styles/LoginStyles';
 import Logo from './Logo';
 
 interface Props {
@@ -26,6 +26,13 @@ interface Props {
 const LoginUser: React.FC<Props> = ({response}) => {
   const [user, setUser] = useState<string>('');
 
+  const login = () => {
+    response({decide: RouteState.validation, user: user});
+  };
+  const setUserName = (username: string) => {
+    setUser(username.toLocaleLowerCase());
+  };
+
   return (
     <KeyboardAvoidingView style={common_styles.safe_area}>
       <StatusBar barStyle="light-content" backgroundColor="#4F68C4" />
@@ -33,7 +40,7 @@ const LoginUser: React.FC<Props> = ({response}) => {
         start={{x: 0, y: 0}}
         end={{x: 0, y: 1}}
         colors={['#4F68C4', '#0C2682']}
-        style={{flex: 1}}>
+        style={login_styles.gradient}>
         <View style={login_styles.login_body}>
           <Logo type={'user'} />
           <View style={common_styles.package}>
@@ -42,17 +49,13 @@ const LoginUser: React.FC<Props> = ({response}) => {
               clearButtonMode={'while-editing'}
               placeholder={'username'}
               placeholderTextColor={'rgba(255,255,255,0.4)'}
-              onSubmitEditing={() =>
-                response({decide: RouteState.validation, user: user})
-              }
-              onChangeText={(text) => setUser(text.toLocaleLowerCase())}
+              onSubmitEditing={login}
+              onChangeText={setUserName}
             />
             <TouchableHighlight
               underlayColor={'transparent'}
               style={login_styles.login_button}
-              onPress={() =>
-                response({decide: RouteState.validation, user: user})
-              }>
+              onPress={login}>
               <Text style={login_styles.login_button_text}>{'Next'}</Text>
             </TouchableHighlight>
           </View>
