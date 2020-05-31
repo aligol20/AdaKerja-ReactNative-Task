27a13a076efe915e43d8566a53568c2ad431d1ca
aligol20 <aligol20@gmail.com>
2020-05-31 19:08:02 +0430

    It's Simple ts file with some enums

---
+++ src/types/Mytypes.ts
@@ -0,0 +1,15 @@
+/**
+ * It's Simple ts file with some enums
+ * I used them to reduce volume of codes and pass props easily
+ * most of them are usable in several places
+ */
+
+export enum RouteState {
+  loading = ' loading',
+  validation = 'validation',
+  logged = 'logged',
+  signIn = 'signIn',
+}
+export enum OverViewType {
+  HomePage = 'HomePage',
+}
